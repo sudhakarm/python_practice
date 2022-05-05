b7e6ba2367b23655f8527f028719e874cf2b8c74
Sudhakar Mangipudi <sudhakar.mangipudi@gmail.com>
2022-05-06 00:29:57 +0530

    Update SubstringWithConcatWords.py

--- challenges/SubstringWithConcatWords.py
+++ challenges/SubstringWithConcatWords.py
@@ -1,21 +1,31 @@
-from itertools import permutations
-
-class Solution:
-    def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        #first make different combinations of given words
-        perm_list = list(map("".join,permutations(words)))
-        outList = []
-        
-        for word in perm_list:
-            index=0
-            while index < len(s):
-                index = s.find(word, index)
-                if index == -1:
-                    break
-                outList.append(index)
-                index += 1    
-        return set(outList) #returns unique list
-
+"""
+This solution collected from https://hecodesit.com/substring-with-concatenation-of-all-words-leetcode/
+look at history for my original solution, which is out of memory for long sets
+"""
+class Solution(object):
+    def findSubstring(self, s, words):
+        """
+        :type s: str
+        :type words: List[str]
+        :rtype: List[int]
+        """
+        dict = collections.defaultdict(int)
+        for w in words:
+            dict[w] += 1
+        l, res = len(words[0]), []
+        for i in range(l):
+            dict1 = dict.copy()
+            j = i
+            while j < len(s) - l + 1:
+                dict1[s[j : j + l]] -= 1
+                while dict1[s[j : j + l]] < 0:
+                    dict1[s[i : i + l]] += 1
+                    i += l
+                j += l
+                if (j - i) / l == len(words):
+                    res.append(i)
+        return res
+    
 s = "barfoothefoobarman"
 words =  ["foo","bar"]
 sol = Solution()
